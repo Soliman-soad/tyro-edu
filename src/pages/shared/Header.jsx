e76ef1f7b711d3bce0087c5bc8484a551c5ae867
Soliman-soad <mdsolimansoad@gmail.com>
2022-10-25 13:07:02 +0600

    route set up done

--- src/pages/shared/Header.jsx
+++ src/pages/shared/Header.jsx
@@ -1,40 +1,275 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Link, NavLink } from 'react-router-dom';
 const Header = () => {
-   
+	const [isMenuOpen, setIsMenuOpen] = useState(false);
+	const activeStyle = {
+		textDecoration: "underline",
+	  };
     return (
-        <header className="p-4 dark:bg-gray-800 dark:text-gray-100">
-	<div className="container flex justify-between h-16 mx-auto">
-		<a rel="noopener noreferrer" href="#" aria-label="Back to homepage" className="flex items-center p-2">
-			<h1 className='uppercase text-4xl font-bold'>
-                tyro
+        <div className="bg-gray-900">
+      <div className="px-4 py-5 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8">
+        <div className="relative flex items-center justify-between">
+          <Link
+            to="/"
+            aria-label="Company"
+            title="Company"
+            className="inline-flex items-center"
+          >
+            <span>
+			<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-10 h-10">
+				<path strokeLinecap="round" strokeLinejoin="round" d="M15.042 21.672L13.684 16.6m0 0l-2.51 2.225.569-9.47 5.227 7.917-3.286-.672zm-7.518-.267A8.25 8.25 0 1120.25 10.5M8.288 14.212A5.25 5.25 0 1117.25 10.5" />
+			</svg>	
+			</span>		
+				<span className='uppercase text-4xl font-bold p-2'>
+			   		tyro
                 <span className='text-teal-700'> EDU</span>
-            </h1>
-		</a>
-		<ul className="items-stretch hidden space-x-3 lg:flex">
-			<li className="flex">
-				<a rel="noopener noreferrer" href="#" className="flex items-center px-4 -mb-1 border-b-2 dark:border-transparent dark:text-violet-400 dark:border-violet-400">Link</a>
-			</li>
-			<li className="flex">
-				<a rel="noopener noreferrer" href="#" className="flex items-center px-4 -mb-1 border-b-2 dark:border-transparent">Link</a>
-			</li>
-			<li className="flex">
-				<a rel="noopener noreferrer" href="#" className="flex items-center px-4 -mb-1 border-b-2 dark:border-transparent">Link</a>
-			</li>
-			<li className="flex">
-				<a rel="noopener noreferrer" href="#" className="flex items-center px-4 -mb-1 border-b-2 dark:border-transparent">Link</a>
-			</li>
-		</ul>
-		<div className="items-center flex-shrink-0 hidden lg:flex">
-			<button className="self-center px-8 py-3 rounded">Sign in</button>
-			<button className="self-center px-8 py-3 font-semibold rounded dark:bg-violet-400 dark:text-gray-900">Sign up</button>
-		</div>
-		<button className="p-4 lg:hidden">
-			<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" className="w-6 h-6 dark:text-gray-100">
-				<path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h16"></path>
-			</svg>
-		</button>
-	</div>
-</header>
+			   </span>
+			   
+          </Link>
+          <ul className="flex items-center hidden space-x-8 lg:flex">
+            <li>
+              <NavLink
+                to="/"
+                aria-label="Our product"
+                title="Our product"
+                className="font-medium tracking-wide text-gray-100 transition-colors duration-200 hover:text-teal-accent-400"
+				style={({ isActive }) =>
+				isActive ? activeStyle : undefined
+			  }
+			  end>
+                Home
+              </NavLink>
+            </li>
+            <li>
+              <NavLink
+                to="/courses"
+                aria-label="Our product"
+                title="Our product"
+                className="font-medium tracking-wide text-gray-100 transition-colors duration-200 hover:text-teal-accent-400"
+				style={({ isActive }) =>
+				isActive ? activeStyle : undefined
+			  }
+			  >
+				Courses
+              </NavLink>
+            </li>
+            <li>
+              <NavLink
+                to="/faq"
+                aria-label="Product pricing"
+                title="Product pricing"
+                className="font-medium tracking-wide text-gray-100 transition-colors duration-200 hover:text-teal-accent-400"
+				style={({ isActive }) =>
+				isActive ? activeStyle : undefined
+			  }
+			  >
+                FAQ
+              </NavLink>
+            </li>
+            <li>
+              <NavLink
+                to="/blog"
+                aria-label="About us"
+                title="About us"
+                className="font-medium tracking-wide text-gray-100 transition-colors duration-200 hover:text-teal-accent-400"
+				style={({ isActive }) =>
+				isActive ? activeStyle : undefined
+			  }
+			  >
+                Blog
+              </NavLink>
+            </li>
+            <li>
+              <NavLink
+                to="/about"
+                aria-label="About us"
+                title="About us"
+                className="font-medium tracking-wide text-gray-100 transition-colors duration-200 hover:text-teal-accent-400"
+				style={({ isActive }) =>
+				isActive ? activeStyle : undefined
+			  }
+			  >
+                About
+              </NavLink>
+            </li>
+            
+          </ul>
+          <ul className="flex items-center hidden space-x-8 lg:flex">
+            <li>
+              <Link
+                to="/login"
+                className="inline-flex items-center justify-center h-12 px-6 font-medium tracking-wide text-white transition duration-200 rounded shadow-md hover:bg-teal-500 bg-teal-700 focus:shadow-outline focus:outline-none"
+                aria-label="Sign up"
+                title="Sign up"
+              >
+                LogIn
+              </Link>
+            </li>
+            <li>
+              <Link
+                to="/register"
+                className="inline-flex items-center justify-center h-12 px-6 font-medium tracking-wide text-white transition duration-200 rounded shadow-md hover:bg-teal-500 bg-teal-700 focus:shadow-outline focus:outline-none"
+                aria-label="Sign up"
+                title="Sign up"
+              >
+                Register
+              </Link>
+            </li>
+          </ul>
+          <div className="lg:hidden">
+            <button
+              aria-label="Open Menu"
+              title="Open Menu"
+              className="p-2 -mr-1 transition duration-200 rounded focus:outline-none focus:shadow-outline"
+              onClick={() => setIsMenuOpen(true)}
+            >
+              <svg className="w-5 text-gray-600" viewBox="0 0 24 24">
+                <path
+                  fill="currentColor"
+                  d="M23,13H1c-0.6,0-1-0.4-1-1s0.4-1,1-1h22c0.6,0,1,0.4,1,1S23.6,13,23,13z"
+                />
+                <path
+                  fill="currentColor"
+                  d="M23,6H1C0.4,6,0,5.6,0,5s0.4-1,1-1h22c0.6,0,1,0.4,1,1S23.6,6,23,6z"
+                />
+                <path
+                  fill="currentColor"
+                  d="M23,20H1c-0.6,0-1-0.4-1-1s0.4-1,1-1h22c0.6,0,1,0.4,1,1S23.6,20,23,20z"
+                />
+              </svg>
+            </button>
+            {isMenuOpen && (
+              <div className="absolute top-0 left-0 w-full">
+                <div className="p-5 bg-white border rounded shadow-sm">
+                  <div className="flex items-center justify-between mb-4">
+                    <div>
+                      <Link
+                        to="/"
+                        aria-label="Company"
+                        title="Company"
+                        className="inline-flex items-center"
+                      >
+                        <span>
+			<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-10 h-10">
+				<path strokeLinecap="round" strokeLinejoin="round" d="M15.042 21.672L13.684 16.6m0 0l-2.51 2.225.569-9.47 5.227 7.917-3.286-.672zm-7.518-.267A8.25 8.25 0 1120.25 10.5M8.288 14.212A5.25 5.25 0 1117.25 10.5" />
+			</svg>	
+			</span>		
+				<span className='uppercase text-4xl font-bold p-2'>
+			   		tyro
+                <span className='text-teal-700'> EDU</span>
+			   </span>
+                      </Link>
+                    </div>
+                    <div>
+                      <button
+                        aria-label="Close Menu"
+                        title="Close Menu"
+                        className="p-2 -mt-2 -mr-2 transition duration-200 rounded hover:bg-gray-200 focus:bg-gray-200 focus:outline-none focus:shadow-outline"
+                        onClick={() => setIsMenuOpen(false)}
+                      >
+                        <svg className="w-5 text-gray-600" viewBox="0 0 24 24">
+                          <path
+                            fill="currentColor"
+                            d="M19.7,4.3c-0.4-0.4-1-0.4-1.4,0L12,10.6L5.7,4.3c-0.4-0.4-1-0.4-1.4,0s-0.4,1,0,1.4l6.3,6.3l-6.3,6.3 c-0.4,0.4-0.4,1,0,1.4C4.5,19.9,4.7,20,5,20s0.5-0.1,0.7-0.3l6.3-6.3l6.3,6.3c0.2,0.2,0.5,0.3,0.7,0.3s0.5-0.1,0.7-0.3 c0.4-0.4,0.4-1,0-1.4L13.4,12l6.3-6.3C20.1,5.3,20.1,4.7,19.7,4.3z"
+                          />
+                        </svg>
+                      </button>
+                    </div>
+                  </div>
+                  <nav>
+                    <ul className="space-y-4">
+                      <li>
+                        <NavLink
+                          to="/"
+                          aria-label="Our product"
+                          title="Our product"
+                          className="font-medium tracking-wide text-gray-700 transition-colors duration-200 hover:text-deep-purple-accent-400"
+						  style={({ isActive }) =>
+						  isActive ? activeStyle : undefined
+						}
+						end>
+                          Home
+                        </NavLink>
+                      </li>
+                      <li>
+                        <NavLink
+                          to="/courses"
+                          aria-label="Our product"
+                          title="Our product"
+                          className="font-medium tracking-wide text-gray-700 transition-colors duration-200 hover:text-deep-purple-accent-400"
+						  style={({ isActive }) =>
+						  isActive ? activeStyle : undefined
+						}
+						>
+                          Courses
+                        </NavLink>
+                      </li>
+                      <li>
+                        <NavLink
+                          to="/faq"
+                          aria-label="Product pricing"
+                          title="Product pricing"
+                          className="font-medium tracking-wide text-gray-700 transition-colors duration-200 hover:text-deep-purple-accent-400"
+						  style={({ isActive }) =>
+						  isActive ? activeStyle : undefined
+						}
+						>
+                          FAQ
+                        </NavLink>
+                      </li>
+                      <li>
+                        <NavLink
+                          to="/blog"
+                          aria-label="About us"
+                          title="About us"
+                          className="font-medium tracking-wide text-gray-700 transition-colors duration-200 hover:text-deep-purple-accent-400"
+						  style={({ isActive }) =>
+						  isActive ? activeStyle : undefined
+						}
+						>
+                          Blog
+                        </NavLink>
+                      </li>
+                      <li>
+                        <NavLink
+                          to="/about"
+                          aria-label="About us"
+                          title="About us"
+                          className="font-medium tracking-wide text-gray-700 transition-colors duration-200 hover:text-deep-purple-accent-400"
+						  style={({ isActive }) =>
+						  isActive ? activeStyle : undefined
+						}
+						>
+                          About
+                        </NavLink>
+                      </li>
+                      <li>
+                        <Link
+                          href="/login"
+                          className="inline-flex items-center justify-center w-full h-12 px-6 font-medium tracking-wide text-white transition duration-200 rounded shadow-md bg-teal-700 mb-2 md:m-0 hover:bg-teal-500 focus:shadow-outline focus:outline-none"
+                          aria-label="Sign up"
+                          title="Sign up"
+                        >
+                          LogIn
+                        </Link>
+                        <Link
+                          href="/register"
+                          className="inline-flex items-center justify-center w-full h-12 px-6 font-medium tracking-wide text-white transition duration-200 rounded shadow-md bg-teal-700 hover:bg-teal-500 focus:shadow-outline focus:outline-none"
+                          aria-label="Sign up"
+                          title="Sign up"
+                        >
+                          Register
+                        </Link>
+                      </li>
+                    </ul>
+                  </nav>
+                </div>
+              </div>
+            )}
+          </div>
+        </div>
+      </div>
+    </div>
     );
 };
 
--- src/routes/router.js
+++ src/routes/router.js
@@ -1,10 +1,47 @@
 import { createBrowserRouter } from "react-router-dom";
 import Main from "../main/Main";
+import About from "../pages/About";
+import Login from "../pages/authenticationPages/Login";
+import Register from "../pages/authenticationPages/Register";
+import Blog from "../pages/Blog";
+import Courses from "../pages/Courses";
+import Faq from "../pages/Faq";
+import Home from "../pages/Home";
 
 const router = createBrowserRouter([
     {
         path:'/',
-        element:<Main/>
+        element:<Main/>,
+        children:[
+            {
+                path:'/',
+                element:<Home/>,
+            },
+            {
+                path:'/courses',
+                element:<Courses/>
+            },
+            {
+                path:'faq',
+                element:<Faq/>
+            },
+            {
+                path:'blog',
+                element:<Blog/>
+            },
+            {
+                path:'about',
+                element:<About/>
+            },
+            {
+                path:'login',
+                element:<Login/>
+            },
+            {
+                path:'register',
+                element:<Register/>
+            }
+        ]
     }
 ])
 export default router;
