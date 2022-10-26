95ef34b77a8bd4869fa55d4d8c5df5b5c62353b3
Soliman-soad <mdsolimansoad@gmail.com>
2022-10-26 09:04:04 +0600

    email login system added

--- src/context/UserContext.js
+++ src/context/UserContext.js
@@ -1,5 +1,5 @@
 import React, { createContext } from 'react';
-import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
+import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword } from "firebase/auth";
 import app from '../firebase/firebase.init';
 
 export const ProfileContext =createContext()
@@ -8,7 +8,10 @@ const UserContext = ({children}) => {
     const register = (email,password) =>{
         return createUserWithEmailAndPassword(auth,email,password)
     }
-    const authInfo ={register}
+    const logIn = (email,password) =>{
+        return signInWithEmailAndPassword(auth,email,password)
+    }
+    const authInfo ={register,logIn}
     return (
         <ProfileContext.Provider value={authInfo}>
             {children}
