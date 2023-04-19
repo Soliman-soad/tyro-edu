4fbdaeb479e2f820270e8d7dc4a6784f706619bd
Soliman-soad <mdsolimansoad@gmail.com>
2023-04-19 10:20:06 +0600

    blog and faq feature change

--- src/pages/Blog.jsx
+++ src/pages/Blog.jsx
@@ -2,24 +2,101 @@ import React from 'react';
 
 const Blog = () => {
     return (
-        <div className='p-5 bg-white h-screen  dark:bg-gray-800'>
-            <div className=' dark:text-white text-gray-800  my-5'>
-                <h1 className=' dark:text-white text-xl font-bold uppercase'> what is cors?</h1>
-                <p>Cross-origin resource sharing (CORS) is a browser mechanism which enables controlled access to resources located outside of a given domain. It extends and adds flexibility to the same-origin policy. However, it also provides potential for cross-domain attacks, if a website's CORS policy is poorly configured and implemented. CORS is not a protection against cross-origin attacks such as cross-site request forgery </p>
-            </div>
-            <div className=' dark:text-white text-gray-800  my-5'>
-                <h1 className=' dark:text-white text-xl font-bold uppercase'>Why are you using firebase? What other options do you have to implement authentication?</h1>
-                <p>Firebase Authentication provides backend services, easy-to-use SDKs, and ready-made UI libraries to authenticate users to your app. It supports authentication using passwords, phone numbers, popular federated identity providers like Google, Facebook and Twitter, and more.</p>
-            </div>
-            <div className=' dark:text-white text-gray-800  my-5'>
-                <h1 className=' dark:text-white text-xl font-bold uppercase'>How does the private route work?</h1>
-                <p> private route is mainly recognition process route  If the user is not authenticated he will be redirected to the login page and the user can only access the authenticated routes If he is Logged in.Under hood it check conditions</p>
-            </div>
-            <div className=' dark:text-white text-gray-800  my-5'>
-                <h1 className=' dark:text-white text-xl font-bold uppercase'>What is Node? How does Node work?</h1>
-                <p> Node.js uses the “Single Threaded Event Loop” architecture to handle multiple clients at the same time.In a multi-threaded request-response model, multiple clients send a request, and the server processes each one before sending the response back. However, multiple threads are used to process concurrent calls. These threads are defined in a thread pool, and each time a request comes in, an individual thread is assigned to handle it.</p>
-            </div>
-        </div>
+        <>
+        {/* ------ */}
+        <div className="p-5 mx-auto sm:p-10 md:p-16 bg-gray-100 text-gray-800">
+	<div className="flex flex-col max-w-3xl mx-auto overflow-hidden rounded">
+		<img src="https://source.unsplash.com/random/480x360" alt="" className="w-full h-60 sm:h-96 bg-gray-500" />
+		<div className="p-6 pb-12 m-4 mx-auto -mt-16 space-y-6 lg:max-w-2xl sm:px-10 sm:mx-12 lg:rounded-md bg-gray-50">
+			<div className="space-y-2">
+				<a rel="noopener noreferrer" href="#" className="inline-block text-2xl font-semibold sm:text-3xl">what is cors?</a>
+				<p className="text-xs text-gray-600">By
+					<span className="text-xs hover:underline">Soliman Alam</span>
+				</p>
+			</div>
+			<div className="text-gray-800">
+            <p>Cross-origin resource sharing (CORS) is a browser mechanism which enables controlled access to resources located outside of a given domain. It extends and adds flexibility to the same-origin policy. However, it also provides potential for cross-domain attacks, if a website's CORS policy is poorly configured and implemented. CORS is not a protection against cross-origin attacks such as cross-site request forgery </p>
+			</div>
+		</div>
+	</div>
+</div>
+        {/* ------ */}
+
+        {/* -------- */}
+        <div className="bg-gray-100 text-gray-900">
+	<div className="container grid grid-cols-12 mx-auto bg-gray-50">
+		<div className="bg-no-repeat bg-cover bg-gray-700 col-span-full lg:col-span-4" style="background-image: url('https://source.unsplash.com/random/640x480'); background-position: center center; background-blend-mode: multiply; background-size: cover;"></div>
+		<div className="flex flex-col p-6 col-span-full row-span-full lg:col-span-8 lg:p-10">
+			<div className="flex justify-start">
+				<span className="px-2 py-1 text-xs rounded-full bg-teal-600 text-gray-50">Firebase</span>
+			</div>
+			<h1 className="text-3xl font-semibold">Why are you using firebase? What other options do you have to implement authentication?</h1>
+			<p className="flex-1 pt-2">Firebase Authentication provides backend services, easy-to-use SDKs, and ready-made UI libraries to authenticate users to your app. It supports authentication using passwords, phone numbers, popular federated identity providers like Google, Facebook and Twitter, and more.</p>
+			
+			<div className="flex items-center justify-between pt-2">
+				<div className="flex space-x-2">
+					<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="w-5 h-5 text-gray-600">
+						<path fillRule="evenodd" d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-6-3a2 2 0 11-4 0 2 2 0 014 0zm-2 4a5 5 0 00-4.546 2.916A5.986 5.986 0 0010 16a5.986 5.986 0 004.546-2.084A5 5 0 0010 11z" clipRule="evenodd"></path>
+					</svg>
+					<span className="self-center text-sm">by Soliman Alam</span>
+				</div>
+			
+			</div>
+		</div>
+	</div>
+</div>
+        {/* -------- */}
+
+        {/* -------- */}
+        <div className="bg-gray-100 text-gray-900">
+	<div className="container grid grid-cols-12 mx-auto bg-gray-50">
+		<div className="bg-no-repeat bg-cover bg-gray-700 col-span-full lg:col-span-4" style="background-image: url('https://source.unsplash.com/random/640x480'); background-position: center center; background-blend-mode: multiply; background-size: cover;"></div>
+		<div className="flex flex-col p-6 col-span-full row-span-full lg:col-span-8 lg:p-10">
+			<div className="flex justify-start">
+				<span className="px-2 py-1 text-xs rounded-full bg-teal-600 text-gray-50">Route</span>
+			</div>
+			<h1 className="text-3xl font-semibold">How does the private route work?</h1>
+			<p className="flex-1 pt-2">private route is mainly recognition process route  If the user is not authenticated he will be redirected to the login page and the user can only access the authenticated routes If he is Logged in.Under hood it check conditions</p>
+			
+			<div className="flex items-center justify-between pt-2">
+				<div className="flex space-x-2">
+					<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="w-5 h-5 text-gray-600">
+						<path fillRule="evenodd" d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-6-3a2 2 0 11-4 0 2 2 0 014 0zm-2 4a5 5 0 00-4.546 2.916A5.986 5.986 0 0010 16a5.986 5.986 0 004.546-2.084A5 5 0 0010 11z" clipRule="evenodd"></path>
+					</svg>
+					<span className="self-center text-sm">by Soliman Alam</span>
+				</div>
+			
+			</div>
+		</div>
+	</div>
+</div>
+        {/* -------- */}
+        {/* -------- */}
+        <div className="bg-gray-100 text-gray-900">
+	<div className="container grid grid-cols-12 mx-auto bg-gray-50">
+		<div className="bg-no-repeat bg-cover bg-gray-700 col-span-full lg:col-span-4" style="background-image: url('https://source.unsplash.com/random/640x480'); background-position: center center; background-blend-mode: multiply; background-size: cover;"></div>
+		<div className="flex flex-col p-6 col-span-full row-span-full lg:col-span-8 lg:p-10">
+			<div className="flex justify-start">
+				<span className="px-2 py-1 text-xs rounded-full bg-teal-600 text-gray-50">Node</span>
+			</div>
+			<h1 className="text-3xl font-semibold">What is Node? How does Node work?</h1>
+			<p className="flex-1 pt-2">Node.js uses the “Single Threaded Event Loop” architecture to handle multiple clients at the same time.In a multi-threaded request-response model, multiple clients send a request, and the server processes each one before sending the response back. However, multiple threads are used to process concurrent calls. These threads are defined in a thread pool, and each time a request comes in, an individual thread is assigned to handle it.</p>
+			
+			<div className="flex items-center justify-between pt-2">
+				<div className="flex space-x-2">
+					<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="w-5 h-5 text-gray-600">
+						<path fillRule="evenodd" d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-6-3a2 2 0 11-4 0 2 2 0 014 0zm-2 4a5 5 0 00-4.546 2.916A5.986 5.986 0 0010 16a5.986 5.986 0 004.546-2.084A5 5 0 0010 11z" clipRule="evenodd"></path>
+					</svg>
+					<span className="self-center text-sm">by Soliman Alam</span>
+				</div>
+			
+			</div>
+		</div>
+	</div>
+</div>
+        {/* -------- */}
+    
+        </>
         
         
     );
--- src/pages/Faq.jsx
+++ src/pages/Faq.jsx
@@ -2,26 +2,36 @@ import React from 'react';
 
 const Faq = () => {
     return (
+    
         <div className='dark:bg-gray-800 bg-white h-screen p-5'>
-            <div>
-                  
-            <div className=' dark:text-white text-gray-800  '>
-                <h1 className=' dark:text-white text-xl font-bold uppercase'>what is artificial intelligence?</h1>
-                <p>the ability of a digital computer or computer-controlled robot to perform tasks commonly associated with intelligent beings. The term is frequently applied to the project of developing systems endowed with the intellectual processes characteristic of humans, such as the ability to reason, discover meaning, generalize, or learn from past experience. Since the development of the digital computer in the 1940s, it has been demonstrated that computers can be programmed to carry out very complex tasks—as, for example, discovering proofs for mathematical theorems or playing chess—with great proficiency. Still, despite continuing advances in computer processing speed and memory capacity, there are as yet no programs that can match human flexibility over wider domains or in tasks requiring much everyday knowledge. On the other hand, some programs have attained the performance levels of human experts and professionals in performing certain specific tasks, so that artificial intelligence in this limited sense is found in applications as diverse as medical diagnosis, computer search engines, and voice or handwriting recognition.</p>
-            </div>
-            <div className=' dark:text-white text-gray-800  my-5'>
-                <h1 className=' dark:text-white text-xl font-bold uppercase'>what is Web3?</h1>
-                <p>Centralization has helped onboard billions of people to the World Wide Web and created the stable, robust infrastructure on which it lives. At the same time, a handful of centralized entities have a stronghold on large swathes of the World Wide Web, unilaterally deciding what should and should not be allowed.
-
-Web3 is the answer to this dilemma. Instead of a Web monopolized by large technology companies, Web3 embraces decentralization and is being built, operated, and owned by its users. Web3 puts power in the hands of individuals rather than corporations. Before we talk about Web3, let's explore how we got here.</p>
-            </div>
-            <div className=' dark:text-white text-gray-800  my-5'>
-                <h1 className=' dark:text-white text-xl font-bold uppercase'>What is a bot?</h1>
-                <p>A bot -- short for robot and also called an internet bot -- is a computer program that operates as an agent for a user or other program or to simulate a human activity. Bots are normally used to automate certain tasks, meaning they can run without specific instructions from humans.
+            {/* ----- */}
+            <section className="bg-gray-100 text-gray-800 dark:text-white ">
+	<div className="container flex flex-col justify-center px-4 py-8 mx-auto md:p-8">
+		<h2 className="mb-12 text-4xl font-bold leading-none text-center sm:text-5xl">Frequently Asked Questions</h2>
+		<div className="divide-y divide-gray-300">
+			<div className="py-6 space-y-2 md:grid md:grid-cols-12 md:gap-8 md:space-y-0">
+				<h3 className="font-semibold md:col-span-5">what is artificial intelligence?</h3>
+				<p className="md:pl-0 md:col-span-7">the ability of a digital computer or computer-controlled robot to perform tasks commonly associated with intelligent beings. The term is frequently applied to the project of developing systems endowed with the intellectual processes characteristic of humans, such as the ability to reason, discover meaning, generalize, or learn from past experience. Since the development of the digital computer in the 1940s, it has been demonstrated that computers can be programmed to carry out very complex tasks—as, for example, discovering proofs for mathematical theorems or playing chess—with great proficiency. Still, despite continuing advances in computer processing speed and memory capacity, there are as yet no programs that can match human flexibility over wider domains or in tasks requiring much everyday knowledge. On the other hand, some programs have attained the performance levels of human experts and professionals in performing certain specific tasks, so that artificial intelligence in this limited sense is found in applications as diverse as medical diagnosis, computer search engines, and voice or handwriting recognition.</p>
+			</div>
+			<div className="py-6 space-y-2 md:grid md:grid-cols-12 md:gap-8 md:space-y-0">
+				<h3 className="font-semibold md:col-span-5">what is Web3?</h3>
+				<p className="md:pl-0 md:col-span-7">Centralization has helped onboard billions of people to the World Wide Web and created the stable, robust infrastructure on which it lives. At the same time, a handful of centralized entities have a stronghold on large swathes of the World Wide Web, unilaterally deciding what should and should not be allowed. Web3 is the answer to this dilemma. Instead of a Web monopolized by large technology companies, Web3 embraces decentralization and is being built, operated, and owned by its users. Web3 puts power in the hands of individuals rather than corporations. Before we talk about Web3, let's explore how we got here.</p>
+			</div>
+			<div className="py-6 space-y-2 md:grid md:grid-cols-12 md:gap-8 md:space-y-0">
+				<h3 className="font-semibold md:col-span-5">Modi dolorem veritatis culpa quos consequuntur beatae itaque excepturi perspiciatis?</h3>
+				<p className="md:pl-0 md:col-span-7">Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut voluptates aspernatur dolores in consequatur doloremque inventore reprehenderit, consequuntur perspiciatis architecto.</p>
+			</div>
+			<div className="py-6 space-y-2 md:grid md:grid-cols-12 md:gap-8 md:space-y-0">
+				<h3 className="font-semibold md:col-span-5">What is a bot?</h3>
+				<p className="md:pl-0 md:col-span-7">A bot -- short for robot and also called an internet bot -- is a computer program that operates as an agent for a user or other program or to simulate a human activity. Bots are normally used to automate certain tasks, meaning they can run without specific instructions from humans.
 
 An organization or individual can use a bot to replace a repetitive task that a human would otherwise have to perform. Bots are also much faster at these tasks than humans. Although bots can carry out useful functions, they can also be malicious and come in the form of malware.</p>
-            </div>
-        </div>
+			</div>
+		</div>
+	</div>
+</section>
+            {/* ----- */}
+            
         </div>
     );
 };
